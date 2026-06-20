41e01365763dc74f9224a2e12a1c180e0b0b5594
pedrohelias <pedrohelias95@hotmail.com>
2026-06-20 15:54:33 -0300

    tela login estilo completa

--- sistematizacao/constants/Colors.ts
+++ sistematizacao/constants/Colors.ts
@@ -5,7 +5,10 @@ export default{
   white: "#fff",
   green: "#99CF1D",
   gray: "#DDDDDD",
-  brown_text: "#593417"
+  brown_text: "#593417",
+  white_card: "#F5EFEB",
+  blue_botton: "#112852",
+  signup_text: "#9c7445"
 
 
 
