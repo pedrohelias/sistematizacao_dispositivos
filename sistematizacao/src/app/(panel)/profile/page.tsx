5b05728c2c8ffd2f2f2357b51054ffc576aefe40
pedrohelias <pedrohelias95@hotmail.com>
2026-06-22 12:16:32 -0300

    atualizaão sessão persistente + botão de logoff

--- sistematizacao/src/app/(panel)/profile/page.tsx
+++ sistematizacao/src/app/(panel)/profile/page.tsx
@@ -1,10 +1,29 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useAuth } from "@/src/contexts/AuthContext";
+import { supabase } from "@/src/lib/supabase";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 export default function Profile(){
+
+    const {setAuth} = useAuth()
+
+
+    async function handleSignOut() {
+        const {error} = await supabase.auth.signOut()
+        setAuth(null)
+        
+        if(error){
+            Alert.alert("Error", "Erro ao sair da conta")
+            return 
+        }
+    }
+
+
     return(
         <View style={styles.container}>
             <Text>Página Perfil</Text>
+            <Button title= "deslogar" onPress={handleSignOut}></Button>
         </View>
+        
     )
 
 }
--- sistematizacao/src/app/_layout.tsx
+++ sistematizacao/src/app/_layout.tsx
@@ -1,23 +1,54 @@
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import {AuthProvider, useAuth} from "../contexts/AuthContext"
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
 
-export default function MainLayout(){
+export default function RootLayout(){
+  return(
+    <AuthProvider>
+      <MainLayout></MainLayout>
+    </AuthProvider>
+
+
+  )
+}
+
+
+function MainLayout(){
+
+  const {setAuth} = useAuth()
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session)=>{
+      console.log("session user", session?.user)
+      if(session){
+        setAuth(session.user)
+        router.replace("/(panel)/profile/page")
+        return
+      }
+
+      setAuth(null);
+      router.replace("/")
+
+    })
+  }, [])
 
   return(
     <Stack>
 
       <Stack.Screen
-        name="index"
-        options={{headerShown: false}}>
+          name="index"
+          options={{headerShown: false}}>
       </Stack.Screen>
 
        <Stack.Screen
-        name="(auth)/signup/page"
-        options={{headerShown: false}}>
+          name="(auth)/signup/page"
+          options={{headerShown: false}}>
       </Stack.Screen>
 
        <Stack.Screen
-        name="(panel)/profile/page"
-        options={{headerShown: false}}>
+          name="(panel)/profile/page"
+          options={{headerShown: false}}>
       </Stack.Screen>
 
     </Stack>
